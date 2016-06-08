e071b3cc5d61d9c9b9525124a274080247a75b1f
erin cherney <erin.t.cherney@gmail.com>
2016-06-08 10:25:27 -0400

    working on magic 8

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-(function () {
+$(function () {
   console.log("linked")
-})();
+
+  $('#shake').click(function(){
+    $('#ball').removeClass();
+    setTimeout(shakeTheBall, 10);
+  });
+
+  $('#flip').click(function(){
+    setTimeout(flipTheBall, 10);
+  })
+
+function shakeTheBall() {
+  $('#ball').addClass("animated shake");
+}
+
+function flipTheBall() {
+  $('#ball').addClass("animated rotateOut")
+}
+
+});
